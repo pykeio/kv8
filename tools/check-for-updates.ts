0f2183a74b58efc9a3d48cc7c28501af33193123
Carson M. <carson@pyke.io>
2026-05-01 16:53:32 -0500

    Auto-update

---
+++ tools/check-for-updates.ts
@@ -0,0 +1,45 @@
+import $ from '@david/dax';
+import { join } from '@std/path';
+
+import { latestCommit, resetUpstream, ROOT, TARGET_DIR, updateSrc, patch } from './_common.ts';
+
+const CARGO_TOML = join(ROOT, 'Cargo.toml');
+const manifest = await Deno.readTextFile(CARGO_TOML);
+const versionLine = manifest.match(/version = "(\d+\.\d+\.\d+)"/)!;
+const currentVersion = versionLine[1];
+
+await $`git fetch`.cwd(TARGET_DIR);
+
+const [ newCommit, latestVersion ] = await latestCommit();
+if (latestVersion === currentVersion) {
+	console.log('Up to date.');
+	Deno.exit(0);
+}
+
+console.log(`Updating to new version: ${latestVersion} (${newCommit}) from ${currentVersion}`);
+
+await resetUpstream();
+
+await $`git checkout ${newCommit}`.cwd(TARGET_DIR);
+await $`git submodule update --init --recursive`.cwd(TARGET_DIR);
+
+await patch(true);
+
+await updateSrc();
+
+await Deno.writeTextFile(
+	CARGO_TOML,
+	manifest.replace(versionLine[0], `version = "${latestVersion}"`)
+);
+
+await $`git add rusty_v8 Cargo.toml`.cwd(ROOT);
+await $`git commit -m "Update to v8 ${latestVersion}"`.cwd(ROOT);
+await $`git push origin +HEAD:refs/heads/autoupdate`.cwd(ROOT);
+await $`git fetch origin autoupdate`.cwd(ROOT);
+
+const res = await $`gh pr view autoupdate --json state`.stdout('piped');
+if (res.code === 0 && res.stdoutJson.state === 'OPEN') {
+	await $`gh pr edit autoupdate --title "Update to v8 ${latestVersion}"`.cwd(ROOT);
+} else {
+	await $`gh pr create --title "Update to v8 ${latestVersion}" --body "" --head pykeio:autoupdate`.cwd(ROOT);
+}
